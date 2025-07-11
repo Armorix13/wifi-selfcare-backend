863cbae2b86e43a47b462d01bd07a3bfca500273
IzRohitoP <rohitreso13@gmail.com>
2025-07-12 02:58:11 +0530

    {feature}:Changes in  the package json

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,6 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,10 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/v1", parentRouter);
 
-// Error Handling
 app.use(errorHandler);
 
-// Start Server
 const startServer = async () => {
     try {
         await connectDB();
